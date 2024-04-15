6621501286163cdfb216e7ef2087b73a281feb6d
Michael Connor Buchan <mikey@blindcomputing.org>
2024-04-15 19:33:32 +0100

    t: Run Entry type
    This represents a simple run, done all in one go, with no recovery.

---
+++ src/main/java/com/stir/cscu9t4practical1/Run.java
@@ -0,0 +1,18 @@
+// This class holds information about a single running / sprinting training
+// session
+package com.stir.cscu9t4practical1;
+
+public class Run extends DistanceTraining {
+
+  public Run(String n, int d, int m, int y, int h, int min, int s, float dist) {
+    super(n, d, m, y, h, min, s, dist); // Call base ctor
+  }
+
+  @Override
+  public String getEntry() {
+    return String.format("%s ran %g km in %02d:%02d:%02d on %02d/%02d/%d\n",
+                         getName(), getDistance(), getHour(), getMin(),
+                         getSec(), getDay(), getMonth(), getYear());
+  }
+
+} // Run
